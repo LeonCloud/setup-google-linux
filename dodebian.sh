e1de2f03c05db77d4c01fa8623dc0e584c61d2f5
LeonCloud <the.diablos@hotmail.it>
2021-09-04 14:42:45 +0200

    modifica link al mio fork, e cambio da gnome a xfce4

--- dodebian.sh
+++ dodebian.sh
@@ -14,12 +14,10 @@ sudo apt-get update
 sudo apt-get upgrade -y
 
 echo '==== Install packages ===='
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y gnome
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y xfce4
 sudo apt-get install -y autocutsel
 sudo apt install -y xrdp
 sudo systemctl enable xrdp
-sudo apt-get install -y gnome-core
-sudo apt-get install -y gnome-panel
 sudo apt-get install -y synaptic
 
 sudo sed -i 's/allowed_users=console/allowed_users=anybody/' /etc/X11/Xwrapper.config
